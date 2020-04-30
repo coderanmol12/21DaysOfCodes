8b5c4b6f50c1dc29106aac8fc0216661780b74c0
ANMOL GUPTA <anmol.gupta@man-es.com>
2020-05-01 03:37:13 +0530

    basic theory added

--- UdemyAdvanceScala/src/scalaplayground/part3/FutureAndPromises.scala
+++ UdemyAdvanceScala/src/scalaplayground/part3/FutureAndPromises.scala
@@ -7,6 +7,16 @@ import scala.util.{Failure, Success}
 object FutureAndPromises extends App {
 
 
+  /**
+   * Future is a placeholder of the actual result
+   * Promise is a value which futures refers for actual result.
+   *
+   * diff bw parallel and concurrent
+   * A system is said to be concurrent if it can support two or more actions **in progress** at the same time.
+   * A system is said to be parallel if it can support two or more actions executing simultaneously.
+   * @return
+   */
+
   // basic case....
   def calcluateMeaningOfLife(): Int = {
     Thread.sleep(2000)
