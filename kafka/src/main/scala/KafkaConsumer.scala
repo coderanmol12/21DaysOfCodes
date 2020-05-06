27843bd747dff21972c385b23736eff6846f0e7b
ANMOL GUPTA <anmol.gupta@man-es.com>
2020-05-06 11:59:59 +0530

    producer added with callbacks

--- kafka/src/main/scala/KafkaConsumer.scala
+++ kafka/src/main/scala/KafkaConsumer.scala
@@ -7,14 +7,14 @@ import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
 object KafkaConsumer {
   def main(args: Array[String]): Unit = {
     val props = new Properties();
-
+    val topic = "first_code_topic"
     props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
     props.put(ConsumerConfig.GROUP_ID_CONFIG, "1");
 
     val consumer = new KafkaConsumer[String, String] (props);
-    consumer.subscribe(Collections.singletonList("first_topic"));
+    consumer.subscribe(Collections.singletonList(topic));
 
     while(true) {
       val records = consumer.poll(500);
---
+++ kafka/src/main/scala/KafkaProducerWithCallbacks.scala
@@ -0,0 +1,29 @@
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{Callback, KafkaProducer, ProducerConfig, ProducerRecord, RecordMetadata}
+
+object KafkaProducerWithCallbacks extends App {
+
+
+  var topic = "first_code_topic"
+
+  val props = new Properties();
+  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+  props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducer");
+  props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+  props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+  props.put(ProducerConfig.ACKS_CONFIG, "all") // can be 1 and 0
+
+  val producer = new KafkaProducer[String, String](props)
+  sendAsync("hello from scala to kafka!!!")
+
+  def sendAsync(value: String):Unit = {
+    val record = new ProducerRecord[String, String](topic, value,"Hii checking new topic")
+    producer.send(record, new Callback {
+      override def onCompletion(metadata: RecordMetadata, exception: Exception): Unit = {
+        println("in a callback method with offset value : "+ metadata.offset())
+      }
+    })
+  }
+  producer.close();
+}
