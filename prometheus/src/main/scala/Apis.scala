300f67454eb022b6751c1dd8e6c58d9fa56f636a
ANMOL GUPTA <anmol.gupta@man-es.com>
2020-05-17 04:48:55 +0530

    port added

--- prometheus/src/main/scala/Apis.scala
+++ prometheus/src/main/scala/Apis.scala
@@ -4,28 +4,30 @@ import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import kamon.Kamon
 import kamon.prometheus.PrometheusReporter
-import kamon.system.SystemMetrics
 
 import scala.concurrent.ExecutionContext
 import scala.io.StdIn
 
+
+/**
+ * @author Anmol Gupta
+ */
 object Apis extends App {
 
-  val host = "0.0.0.0"
-  val port = "9000"
+  val host = "localhost"
+  val port = "9100"
 
   Kamon.addReporter(new PrometheusReporter())
-  implicit val system: ActorSystem = ActorSystem("helloworld")
+  implicit val system: ActorSystem = ActorSystem("metricsystem")
   implicit val executor: ExecutionContext = system.dispatcher
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
-  def route = path("hello") {  get {
-    Metrics.counter("H101_W")
-    println("hello")
+  def route = path("push"/ "metrics") {  get {
+    Kamon.counter("DATA_INGESTED").increment()   // Metrics
     complete("Hello, Worlds!") }
 
   }
-  val bindingFuture = Http().bindAndHandle(route, "localhost", 9100)
+  val bindingFuture = Http().bindAndHandle(route, host, port)
 
   println(s"Server online at http://localhost:9100/\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
